e4665cd3e7b2e7e7c3547f037c1111d2ae56aa36
adi-123kumar <128770454+adi-123kumar@users.noreply.github.com>
2024-03-17 12:43:37 +0000

    type conversion of datatype and type of datatype

--- type_conversion.js
+++ type_conversion.js
@@ -1,7 +1,30 @@
-let score = "33"
-console.log(typeof score);
-console.log(typeof(score));
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
 
 // coverting the string into number
-let intvalue=Number(score)
-console.log(typeof(intvalue))
+// let intvalue=Number(score)
+
+// console.log(typeof(intvalue))
+// type of vla       result
+// 1.undefined        undefined
+// 2.Null             object
+// 3.Boolean          boolean
+// 4.Number           number
+// 5.Stirng          string
+// if a function is stored in a variable it will return an object function.
+// ex: 
+// const fun= function(){
+//     console.log("hello world")
+// }
+// console.log(typeof fun) >> it will return a function object as an output.
+// console.log(typeof null);   >> it will return an object 
+// console.log(typeof undefined);
+
+// const arr = ["aditya","anay","rishu","krishna"];
+// console.log(typeof arr)   >> it will return an object
+
+// function fn(){
+//     console.log("hello world")
+// }
+// console.log(typeof fn);  >> it will also return function;
