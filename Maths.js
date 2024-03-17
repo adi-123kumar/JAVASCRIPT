74fdb54339cd16ec028529b6d2decbc0094de8a6
adi-123kumar <128770454+adi-123kumar@users.noreply.github.com>
2024-03-17 13:40:59 +0000

    Maths different methods in js

---
+++ Maths.js
@@ -0,0 +1,31 @@
+// using Math is js
+
+// printing the floor and celling value
+// let num=4.4;
+// console.log(Math.floor(num))  
+// console.log(Math.ceil(num));
+
+// printing the absolute value >> always print the positive value
+// let num2=-45;
+// console.log(Math.abs(num2))
+
+// printing the power and square root of any number.
+// console.log(Math.sqrt(16))
+// console.log(Math.pow(2,3))
+
+// to generate a random value we using random() function >> it will only give number between 0 to 1
+// console.log(Math.random())
+
+// for printing the random value above 0 to 1 we have to multiply the generated random value with 10 and add 1 ;
+// console.log((Math.random()*10)+1)
+
+// // finding the celling and floor value of a random number
+// console.log(Math.ceil((Math.random()*10)+1))
+// console.log(Math.floor((Math.random()*10)+1))
+
+// console.log(Math.PI)
+
+
+
+
+
